cfbcb165dcbd8dbf255fc5b34040b5eff23a7181
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-03-19 14:36:14 +0000

    Created ability to authenticate a user password

--- utils/hash.go
+++ utils/hash.go
@@ -6,3 +6,9 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
+
+func AuthenticatePassword(password string, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	// return true is no error else false
+	return err == nil
+}
