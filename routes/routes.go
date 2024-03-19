8a376309ff8bd8424b38a5a37d52dd93c71d7d95
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-03-19 11:45:06 +0000

    Updated the delete API path

--- routes/routes.go
+++ routes/routes.go
@@ -7,11 +7,11 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/users/:id", getUser)
 	server.POST("/users", createUser)
 	server.PUT("/users/:id", updateUser)
-	server.POST("/users/delete/:id", deleteUser)
+	server.DELETE("/users/:id", deleteUser)
 
 	server.GET("/films", getFilms)
 	server.GET("/films/:id", getFilm)
 	server.POST("/films", createFilm)
 	server.PUT("/films/:id", updateFilm)
-	server.POST("/films/delete/:id", deleteFilm)
+	server.DELETE("/films/:id", deleteFilm)
 }
