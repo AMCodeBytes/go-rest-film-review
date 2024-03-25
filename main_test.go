b4c8b820b631d70c9bdc0baf83c1600296648f14
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-03-25 18:28:38 +0000

    Created ability to test the login functionality

--- main_test.go
+++ main_test.go
@@ -9,11 +9,16 @@ import (
 
 	"github.com/AMCodeBytes/go-rest-film-review/models"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 )
 
-type APITestSuite struct {
-	suite.Suite
+// Helper function to create a user
+func CreateUser() {
+
+}
+
+// Helper function to create a film
+func CreateFilm() {
+
 }
 
 func TestSignUpRoute(t *testing.T) {
@@ -37,18 +42,31 @@ func TestSignUpRoute(t *testing.T) {
 func TestLoginRoute(t *testing.T) {
 	router := setupRouter()
 	w := httptest.NewRecorder()
+	rec := httptest.NewRecorder()
 
 	user := models.User{
+		Name:     "First Second",
 		Email:    "email@test.com",
 		Password: "Password123!",
 	}
 
 	jsonValue, _ := json.Marshal(user)
 
-	req, _ := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonValue))
-
+	req, _ := http.NewRequest("POST", "/users", bytes.NewBuffer(jsonValue))
 	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+	user = models.User{
+		Email:    "email@test.com",
+		Password: "Password123!",
+	}
+
+	jsonValue, _ = json.Marshal(user)
+
+	req, _ = http.NewRequest("POST", "/login", bytes.NewBuffer(jsonValue))
+
+	router.ServeHTTP(rec, req)
+	assert.Equal(t, http.StatusOK, rec.Code)
 }
 
 func TestGetUsersRoute(t *testing.T) {
